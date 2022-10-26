ae8dc481dbd2d624ca3801a3737321a8a8941507
Brok3Turtl3 <brok3turtl3@gmail.com>
2022-10-26 09:29:33 -0400

    quick commit

---
+++ constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x9958F7b3e3FF70401D1C566f5Ef8d0b1ac8E823e";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
